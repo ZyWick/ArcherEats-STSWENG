b16332567584cc2cf330be3f351009c2a300c687
Aki Guanzon <83754370+akiguanzon@users.noreply.github.com>
2024-03-31 22:55:22 +0800

    Sort and filter MERGE (#4)
    * + sort and filter buttons
    
    modify as you like
    
    * minor update to mongodb version
    
    * preliminary changes
    
    * Update index.hbs
    
    * Update index.hbs
    
    * Made changes to frontend
    
    * add backend file (empty) for establishment page filter
    
    * removed code from home-filter
    
    * prototype filter
    
    * added note to add establishment
    
    * updated tag dropdown
    
    dropdowns are now dynamic and show tags based on what's on the database
    
    * added ratings filter on home page
    
    * review filter on establishment page added
    
    Let's you only display reviews with specific scores
    
    * added order sort to home page
    
    * removed comments
    
    * removed comments
    
    * prototype sort for establishment page
    
    * review order for establishment page
    
    * added clear filters
    
    clear filter will refresh the page, returning it into it's default state
    
    * removed prototype code
    
    * multi tag filter updated
    
    * updated hidden secondary tag functionality
    
    * removed unnecessary code
    
    * changed comments
    
    * fixed numerous bugs
    
    * automated tests
    
    ---------
    
    Co-authored-by: Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
    Co-authored-by: Ryan Nicholas <nichotaino@gmail.com>

--- test/filter/estab_filter.test.js
+++ test/filter/estab_filter.test.js
@@ -1,3 +1,4 @@
+
 const filterFunctions = require("./estab_filter");
 
 describe('ascending or descending REVIEWS IN ESTAB rating filter', () => {
@@ -48,4 +49,56 @@ describe('exact REVIEWS IN ESTAB rating filter', () => {
         expect(results).toEqual([2.5]);
         expect(results1).toEqual([2.3, 2.1]);
     });
+=======
+const filterFunctions = require("./estab_filter");
+
+describe('ascending or descending REVIEWS IN ESTAB rating filter', () => {
+    it('should return the REVIEWS IN ESTAB according to highest to lowest rating filter', () => {
+
+        var storeProductsNum = [4.8, 4.5, 4.0, 3.4, 4.5, 4.5, 5.0, 2.5, 1.0];
+        var storeProductsNum2 = [1.2, 4.3, 4.2, 2.3, 2.1, 5.0];
+
+        var results = filterFunctions.orderFilter('highest', storeProductsNum, storeProductsNum2)
+
+        expect(results[0]).toEqual(storeProductsNum.sort().reverse());
+        expect(results[1]).toEqual(storeProductsNum2.sort().reverse());
+    });
+
+    it('should return the REVIEWS IN ESTAB according to lowest to highest rating filter', () => {
+
+        var storeProductsNum = [4.8, 4.5, 4.0, 3.4, 4.5, 4.5, 5.0, 2.5, 1.0];
+        var storeProductsNum2 = [1.2, 4.3, 4.2, 2.3, 2.1, 5.0];
+
+        var results = filterFunctions.orderFilter('lowest', storeProductsNum, storeProductsNum2)
+
+        expect(results[0]).toEqual(storeProductsNum.sort());
+        expect(results[1]).toEqual(storeProductsNum2.sort());
+    });
+});
+
+describe('exact REVIEWS IN ESTAB rating filter', () => {
+    it('should return the REVIEWS IN ESTAB according to 4.0 ratings', () => {
+
+        var storeProductsNum = [4.8, 4.5, 4.0, 3.4, 4.5, 4.5, 5.0, 2.5, 1.0];
+        var storeProductsNum2 = [1.2, 4.3, 4.2, 2.3, 2.1, 5.0];
+
+        var results = filterFunctions.reviewFilter(4.0, storeProductsNum);
+        var results1 = filterFunctions.reviewFilter(4.0, storeProductsNum2);
+
+        expect(results).toEqual([4.8, 4.5, 4.0, 4.5, 4.5]);
+        expect(results1).toEqual([4.3, 4.2]);
+    });
+
+    it('should return the REVIEWS IN ESTAB according to 2.0 ratings', () => {
+
+        var storeProductsNum = [4.8, 4.5, 4.0, 3.4, 4.5, 4.5, 5.0, 2.5, 1.0];
+        var storeProductsNum2 = [1.2, 4.3, 4.2, 2.3, 2.1, 5.0];
+
+        var results = filterFunctions.reviewFilter(2.0, storeProductsNum);
+        var results1 = filterFunctions.reviewFilter(2.0, storeProductsNum2);
+
+        expect(results).toEqual([2.5]);
+        expect(results1).toEqual([2.3, 2.1]);
+    });
+
 });
